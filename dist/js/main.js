38ac4030873fd9f6bfd1355aeec191707f472f43
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-05-12 16:43:28 -0600

    feat: current loction class

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
+
+
+
+
+const initApp = () => {
+    // add listeners
+    const geoButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click", getGeoWeather);
+
+    // set up
+
+    // load weather
+}
+
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+    if (event) {
+        if (event.type === "click") {
+            const mapIcon = document.querySelector(".fa-map-marker-alt")
+            addSpinner(mapIcon)
+        }
+        if (!navigator.geolocation) return geoError();
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+}
+
