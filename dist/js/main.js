5dff35650a1c5398a6ae1ca30db7374251e7e8e5
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-05-12 17:16:35 -0600

    feat: spinner animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,30 +1,26 @@
 import CurrentLocation from "./CurrentLocation.js";
+import { addSpinner } from "./domFunctions.js";
 const currentLoc = new CurrentLocation();
 
-
-
-
 const initApp = () => {
-    // add listeners
-    const geoButton = document.getElementById("getLocation");
-    geoButton.addEventListener("click", getGeoWeather);
+  // add listeners
+  const geoButton = document.getElementById("getLocation");
+  geoButton.addEventListener("click", getGeoWeather);
 
-    // set up
+  // set up
 
-    // load weather
-}
-
+  // load weather
+};
 
 document.addEventListener("DOMContentLoaded", initApp);
 
 const getGeoWeather = (event) => {
-    if (event) {
-        if (event.type === "click") {
-            const mapIcon = document.querySelector(".fa-map-marker-alt")
-            addSpinner(mapIcon)
-        }
-        if (!navigator.geolocation) return geoError();
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  if (event) {
+    if (event.type === "click") {
+      const mapIcon = document.querySelector(".fa-map-marker-alt");
+      addSpinner(mapIcon);
     }
-}
-
+    if (!navigator.geolocation) return geoError();
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  }
+};
