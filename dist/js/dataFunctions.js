3f2da31cdc3161bf1afaf10f0ff67b85d3739661
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-05-14 14:50:55 -0600

    feat: defined api function

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,3 +1,5 @@
+const WEATHER_API_KEY = ''
+
 export const setLocationObject = (locationObj, coordsObj) => {
   const { lat, lon, name, unit } = coordsObj;
   locationObj.setLat(lat);
@@ -10,6 +12,13 @@ export const setLocationObject = (locationObj, coordsObj) => {
 
 export const getHomeLocation = () => {
   return localStorage.getItem("defaultWeatherLocation");
+};
+
+export const getCoordsFromApi = async (entryText, units) => {
+  const regex = /^\d+$/g;
+  const flag = regex.test(entryText) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+
 };
 
 export const cleanText = (text) => {
