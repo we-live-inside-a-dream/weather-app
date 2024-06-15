89225858fb52b696627306652d7ddfe73aad6362
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-15 09:00:59 -0600

    fix

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -1,13 +1,32 @@
-let fetch;
+// let fetch;
 
-export async function handler(event, context) {
-  if (!fetch) {
-    fetch = (await import("node-fetch")).default;
-  }
+// export async function handler(event, context) {
+//   if (!fetch) {
+//     fetch = (await import("node-fetch")).default;
+//   }
 
+//   const params = JSON.parse(event.body);
+//   const { lat, lon, units } = params;
+//   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${process.env.WEATHER_API_KEY}`;
+//   try {
+//     const weatherStream = await fetch(url);
+//     const weatherJson = await weatherStream.json();
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(weatherJson),
+//     };
+//   } catch (err) {
+//     return { statusCode: 422, body: err.stack };
+//   }
+// }
+const fetch = require("node-fetch");
+
+const { WEATHER_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { lat, lon, units } = params;
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${process.env.WEATHER_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
   try {
     const weatherStream = await fetch(url);
     const weatherJson = await weatherStream.json();
@@ -18,4 +37,4 @@ export async function handler(event, context) {
   } catch (err) {
     return { statusCode: 422, body: err.stack };
   }
-}
+};
